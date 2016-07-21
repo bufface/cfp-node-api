869fd3cfb47a3201f267d1d114d5b871ccf859c0
Cristian Buffa <bufface@gmail.com>
2016-07-21 16:57:50 -0300

    Video 8: Buscar pelicula

--- test/movie.js
+++ test/movie.js
@@ -98,4 +98,40 @@ describe('La ruta de Peliculas', function () {
         }, done)
     })
   })
+
+  describe('Una peticion GET /:id', function () {
+    it('Deberia obtener una sola pelicula', function (done) {
+      let movie_id
+      let movie = {
+        title: 'Her',
+        year: '2013'
+      }
+
+      request
+        .post('/movie')
+        .set('Accept', 'application/json')
+        .send(movie)
+        .expect(201)
+        .expect('Content-Type',  /application\/json/)
+        .then((res) => {
+          movie_id = res.body.movie._id
+
+          return request
+            .get('/movie/' + movie_id)
+            .set('Accept', 'application/json')
+            .expect(200)
+            .expect('Content-Type',  /application\/json/)
+        })
+        .then((res) => {
+          let body = res.body
+          expect(body).to.have.property('movie')
+
+          movie = body.movie
+          expect(movie).to.have.property('_id', movie_id)
+          expect(movie).to.have.property('title', 'Her')
+          expect(movie).to.have.property('year', '2013')
+          done()
+        }, done)
+    })
+  })
 })
