12fe0427a53fa3fd24ac8dfd0da7dada7ac28b38
Cristian Buffa <bufface@gmail.com>
2016-07-22 16:32:01 -0300

    Video 15: Pruebas con persistencia

---
+++ test/user.js
@@ -0,0 +1,47 @@
+"use strict"
+
+let request = require('supertest-as-promised')
+const api = require('../app')
+const host = api
+const mongoose = require('mongoose')
+const config = require('../lib/config')
+
+request = request(host)
+
+describe('Ruta para los usuarios', function() {
+  describe('POST /', function() {
+
+    before(() => {
+      mongoose.connect(config.database)
+    })
+
+    after((done) => {
+      mongoose.disconnect(done)
+      mongoose.models = {}
+    })
+
+    it.only('deberia crear un nuevo usuario', function(done) {
+      let user = {
+        'username': 'Cristian',
+        'password': 'cr1st14n'
+      }
+
+      request
+      .post('/user')
+      .set('Accept', 'application/json')
+      .set(user)
+      .expect(201)
+      .expect('Content-Type',  /application\/json/)
+      .end((err, res) => {
+        let body = res.body
+        expect(body).to.have.property('user')
+
+        let user = body.user
+        expect(user).to.have.property('_id')
+        expect(user).to.have.property('password')
+        expect(user).to.have.property('username', 'Cristian')
+        done(err)
+      })
+    })
+  })
+})
