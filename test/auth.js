3c544853e7cc734f726c165f37a11d6629d1c644
Cristian Buffa <bufface@gmail.com>
2016-07-25 10:35:11 -0300

    Fix del Video 18

---
+++ test/auth.js
@@ -0,0 +1,41 @@
+"use strict"
+
+let request = require('supertest-as-promised')
+const api = require('../app')
+const host = api
+
+request = request(host)
+
+describe('Ruta para los auth', function() {
+  describe('POST /', function() {
+    it.only('deberia autenticar un usuario', function(done) {
+      let user = {
+        'username': 'Cristian',
+        'password': 'cr1st14n'
+      }
+
+      request
+        .post('/user')
+        .set('Accept', 'application/json')
+        .send(user)
+        .expect(201)
+        .expect('Content-Type',  /application\/json/)
+        .then((res) => {
+          user = res.body.user
+
+          return request
+                  .post('/auth')
+                  .set('Accept', 'application/json')
+                  .send(user)
+                  .expect(201)
+                  expect('Content-Type',  /application\/json/)
+        })
+        .then((res) => {
+          let body = res.body
+
+          expect(body).to.have.property('token')
+          done()
+        }, done)
+    })
+  })
+})
